19ab361d6faa75ee9c828ae48949a652b115d6c9
cadencranford12133 <caden.cranford@ptaaschool.net>
2022-04-01 08:09:16 -0500

    sdg

--- pong.py
+++ pong.py
@@ -1,6 +1,5 @@
-
-  
 import turtle as trtl
+import random as Rand
 
 wn = trtl.Screen()
 wn.bgcolor('black')
@@ -10,7 +9,7 @@ wn.bgpic(background)
 
 # pong ball and walls
 wall_width, wall_height = .5, 5
-border_width, border_height = 40, .5
+border_width, border_height = 80, .5
 
 pong = trtl.Turtle()
 pong.up()
@@ -26,8 +25,6 @@ wall4.shapesize(border_height, border_width)
 wall4.goto(525, 0)
 wall4.seth(90)
 
-
-
 wall3= trtl.Turtle ()
 wall3.up()
 wall3.shape('square')
@@ -35,7 +32,6 @@ wall3.color('white')
 wall3.shapesize(border_height, border_width)
 wall3.goto(-525, 0)
 wall3.seth(90)
-
 
 wall2 = trtl.Turtle()
 wall2.up()
@@ -69,13 +65,24 @@ bttm_border.shapesize(border_width, border_height)
 bttm_border.goto(0, 300)
 bttm_border.seth(90)
 
-# hide all turtles before game starts
-wall1.ht()
-pong.ht()
-wall2.ht()
+scorecount1 = trtl.Turtle()
+scorecount1.up()
+scorecount1.goto(50, 250)
+scorecount1.pd()
+scorecount1.ht()
+
+scorecount2 = trtl.Turtle()
+scorecount2.up()
+scorecount2.goto(-150, 250)
+scorecount2.pd()
+scorecount2.ht()
 
 curser_size = 20
 angle = 0
+poolean = True
+bluescore = 0
+redscore = 0
+font_setup = ("Arial", 20, "normal")
 
 # move buttons
 def up():
@@ -86,6 +93,39 @@ def Ln():
     wall2.bk(10)
 def dn():
     wall1.bk(10)
+
+# fun buttons
+def reset():
+    pong.goto(0, 0)
+    pong.seth(0)
+def bugfix():
+    pong.seth(Rand.randint(0, 360))
+def funbutton():
+    global angle
+    global poolean
+    global bluescore
+    global redscore
+    poolean = False
+    while poolean == False:
+        pong.fd(5)
+        if paddle_collision(pong, wall1):
+            bugfix()
+        if paddle_collision(pong, wall2):
+            bugfix()
+        if hrzntl_brdr_colide(pong, top_border):
+            pong.seth(Rand.randint(0, 180))
+        if hrzntl_brdr_colide(pong, bttm_border):
+            pong.seth(Rand.randint(-180, 0))
+        if scorebordercoll(pong, wall3):
+            redscore += 1
+            scorecount1.clear()
+            scorecount1.write(redscore, font=font_setup)
+            reset()
+        if scorebordercoll(pong, wall4):
+            bluescore += 1
+            scorecount2.clear()
+            scorecount2.write(bluescore, font=font_setup)
+            reset()
 
 # start button
 strtbttn = "StartButton.gif"
@@ -101,13 +141,14 @@ def stort():
 
 def run_pong():
     global angle
-    while True:
+    global bluescore
+    global redscore
+    while poolean == True:
         pong.fd(5)
         # detect collisions between pong ball and the walls
         if paddle_collision(pong, wall1):
             pong.bk(5)
             angle = (pong.heading() + wall1.ycor() * -1)
-            # abs(pong.ycor() - wall1.ycor())
             if angle == 0.0:
                 angle = 180
             pong.seth(angle)
@@ -115,22 +156,24 @@ def run_pong():
         if paddle_collision(pong, wall2):
             pong.bk(5)
             angle = (pong.heading() + wall2.ycor() * -1)
+            if angle == 180:
+                angle = 0
             pong.seth(angle)
         if hrzntl_brdr_colide(pong, top_border):
-            pong.bk(5)
-            pong.seth(angle - angle/2)
-            print(angle - angle/2)
+            pong.seth(Rand.randint(0, 180))
         if hrzntl_brdr_colide(pong, bttm_border):
-            pong.bk(5)
-            pong.seth(angle - angle/2)
-            print(angle - angle/2)
-        if paddle_collision(pong, wall3):
-            print("point")
-            pong.goto(0,0)  
-        if paddle_collision(pong, wall4):
-            print("point") 
-            pong.goto(0,0)  
-       
+            pong.seth(Rand.randint(-180, 0))
+        if scorebordercoll(pong, wall3):
+            redscore += 1
+            scorecount1.clear()
+            scorecount1.write(redscore, font=font_setup)
+            reset()
+        if scorebordercoll(pong, wall4):
+            bluescore += 1
+            scorecount2.clear()
+            scorecount2.write(bluescore, font=font_setup)
+            reset()
+
 
 def paddle_collision(a, b):
     return abs(a.xcor() - b.xcor()) < curser_size/2 + wall_width/2 and abs(a.ycor() - b.ycor()) < curser_size/2 + wall_height * curser_size
@@ -138,10 +181,16 @@ def paddle_collision(a, b):
 def hrzntl_brdr_colide(a, b):
     return abs(a.xcor() - b.xcor()) < curser_size/2 + border_width * curser_size and abs(a.ycor() - b.ycor()) < curser_size/2 + border_height/2
 
+def scorebordercoll(a, b):
+    return abs(a.xcor() - b.xcor()) < curser_size/2 + wall_width * curser_size and abs(a.ycor() - b.ycor()) < curser_size/2 + 999/2
+
 wn.onkeypress(up,"Up")
-wn.onkeypress(dn, "Down")
+wn.onkeypress(dn,"Down")
 wn.onkeypress(LL,"w")
 wn.onkeypress(Ln,"s") 
+wn.onkeypress(bugfix,"slash")
+wn.onkeypress(funbutton,"f")
 wn.onkeypress(stort, "space")
+wn.onkeypress(reset, "r")
 wn.listen()
 wn.mainloop()
