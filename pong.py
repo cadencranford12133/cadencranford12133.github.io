b5f299d2e47a89dc70ee7bcc95eb0975ad387b6f
cadencranford12133 <caden.cranford@ptaaschool.net>
2022-04-04 09:15:06 -0500

    ftgf

--- pong.py
+++ pong.py
@@ -1,37 +1,45 @@
 import turtle as trtl
 import random as Rand
 
+# game screen setup
 wn = trtl.Screen()
 wn.bgcolor('black')
 background = "maze3.gif"
 wn.addshape(background)
 wn.bgpic(background)
+endscreen = 'end credits.gif'
+wn.addshape(endscreen)
 
 # pong ball and walls
 wall_width, wall_height = .5, 5
 border_width, border_height = 80, .5
 
+# pong ball
 pong = trtl.Turtle()
 pong.up()
 pong.shape('circle')
 pong.color('black')
 pong.shapesize(float(.5))
+pong.speed("fastest")
 
+# paddles and walls
 wall4= trtl.Turtle ()
 wall4.up()
 wall4.shape('square')
-wall4.color('white')
+wall4.ht()
 wall4.shapesize(border_height, border_width)
 wall4.goto(525, 0)
 wall4.seth(90)
+wall4.speed("fastest")
 
 wall3= trtl.Turtle ()
 wall3.up()
 wall3.shape('square')
-wall3.color('white')
+wall3.ht()
 wall3.shapesize(border_height, border_width)
 wall3.goto(-525, 0)
 wall3.seth(90)
+wall3.speed("fastest")
 
 wall2 = trtl.Turtle()
 wall2.up()
@@ -40,6 +48,7 @@ wall2.color('blue')
 wall2.shapesize(wall_width, wall_height)
 wall2.goto(-350, 0)
 wall2.seth(90)
+wall2.speed("fastest")
 
 wall1 = trtl.Turtle()
 wall1.up()
@@ -48,35 +57,42 @@ wall1.color('red')
 wall1.shapesize(wall_width, wall_height)
 wall1.goto(350, 0)
 wall1.seth(90)
+wall1.speed("fastest")
 
 top_border = trtl.Turtle()
 top_border.up()
 top_border.shape('square')
-top_border.color('white')
+top_border.ht()
 top_border.shapesize(border_width, border_height)
 top_border.goto(0, -300)
 top_border.seth(90)
+top_border.speed("fastest")
 
 bttm_border = trtl.Turtle()
 bttm_border.up()
 bttm_border.shape('square')
-bttm_border.color('white')
+bttm_border.ht()
 bttm_border.shapesize(border_width, border_height)
 bttm_border.goto(0, 300)
 bttm_border.seth(90)
+bttm_border.speed("fastest")
 
-scorecount1 = trtl.Turtle()
-scorecount1.up()
-scorecount1.goto(50, 250)
-scorecount1.pd()
-scorecount1.ht()
+# score counters
+scorer1 = trtl.Turtle()
+scorer1.up()
+scorer1.goto(50, 250)
+scorer1.pd()
+scorer1.ht()
+scorer1.speed("fastest")
 
-scorecount2 = trtl.Turtle()
-scorecount2.up()
-scorecount2.goto(-150, 250)
-scorecount2.pd()
-scorecount2.ht()
+scorer2 = trtl.Turtle()
+scorer2.up()
+scorer2.goto(-150, 250)
+scorer2.pd()
+scorer2.ht()
+scorer2.speed("fastest")
 
+# variables for the game
 curser_size = 20
 angle = 0
 poolean = True
@@ -109,36 +125,30 @@ def funbutton():
     while poolean == False:
         pong.fd(5)
         if paddle_collision(pong, wall1):
+            pong.bk(5)
             bugfix()
         if paddle_collision(pong, wall2):
+            pong.bk(5)
             bugfix()
         if hrzntl_brdr_colide(pong, top_border):
+            pong.bk(5)
             pong.seth(Rand.randint(0, 180))
         if hrzntl_brdr_colide(pong, bttm_border):
+            pong.bk(5)
             pong.seth(Rand.randint(-180, 0))
         if scorebordercoll(pong, wall3):
             redscore += 1
-            scorecount1.clear()
-            scorecount1.write(redscore, font=font_setup)
+            scorer1.clear()
+            scorer1.write(redscore, font=font_setup)
             reset()
         if scorebordercoll(pong, wall4):
             bluescore += 1
-            scorecount2.clear()
-            scorecount2.write(bluescore, font=font_setup)
+            scorer2.clear()
+            scorer2.write(bluescore, font=font_setup)
             reset()
-
-#endgame
-endgame = trtl.Turtle()
-wall1.st()
-wall2.st()
-pong.st()
-top_border.st()
-run_pong()
-
-
-
-
-
+def stop():
+    wn.clear()
+    wn.bgpic(endscreen)
 
 # start button
 strtbttn = "StartButton.gif"
@@ -152,42 +162,50 @@ def stort():
     top_border.st()
     run_pong()
 
+# game runs
 def run_pong():
     global angle
     global bluescore
     global redscore
     while poolean == True:
         pong.fd(5)
-        # detect collisions between pong ball and the walls
+        # detect collisions between the pong ball and the walls
         if paddle_collision(pong, wall1):
             pong.bk(5)
-            angle = (pong.heading() + wall1.ycor() * -1)
-            if angle == 0.0:
+            if pong.ycor() + wall1.ycor() == 0.0:
                 angle = 180
-            pong.seth(angle)
+                pong.seth(angle)
+            elif angle == 0.0:
+                angle = 180
+                pong.seth(angle)
+            else:
+                angle = Rand.randint(135, 225)
+                pong.seth(angle)
             print(angle)
         if paddle_collision(pong, wall2):
-            pong.bk(5)
-            angle = (pong.heading() + wall2.ycor() * -1)
             if angle == 180:
-                angle = 0
-            pong.seth(angle)
+                angle = 0.0
+                pong.seth(angle)
+            else:
+                angle = Rand.randint(45, 315)
+                pong.seth(angle)
+            print(angle)
         if hrzntl_brdr_colide(pong, top_border):
             pong.seth(Rand.randint(0, 180))
         if hrzntl_brdr_colide(pong, bttm_border):
             pong.seth(Rand.randint(-180, 0))
         if scorebordercoll(pong, wall3):
             redscore += 1
-            scorecount1.clear()
-            scorecount1.write(redscore, font=font_setup)
+            scorer1.clear()
+            scorer1.write(redscore, font=font_setup)
             reset()
         if scorebordercoll(pong, wall4):
             bluescore += 1
-            scorecount2.clear()
-            scorecount2.write(bluescore, font=font_setup)
+            scorer2.clear()
+            scorer2.write(bluescore, font=font_setup)
             reset()
 
-
+# collision functions
 def paddle_collision(a, b):
     return abs(a.xcor() - b.xcor()) < curser_size/2 + wall_width/2 and abs(a.ycor() - b.ycor()) < curser_size/2 + wall_height * curser_size
 
@@ -197,6 +215,7 @@ def hrzntl_brdr_colide(a, b):
 def scorebordercoll(a, b):
     return abs(a.xcor() - b.xcor()) < curser_size/2 + wall_width * curser_size and abs(a.ycor() - b.ycor()) < curser_size/2 + 999/2
 
+# key press sensors
 wn.onkeypress(up,"Up")
 wn.onkeypress(dn,"Down")
 wn.onkeypress(LL,"w")
@@ -205,6 +224,6 @@ wn.onkeypress(bugfix,"slash")
 wn.onkeypress(funbutton,"f")
 wn.onkeypress(stort, "space")
 wn.onkeypress(reset, "r")
+wn.onkeypress(stop, "Escape")
 wn.listen()
 wn.mainloop()
-wn.onkeypress(endgame,"e")
