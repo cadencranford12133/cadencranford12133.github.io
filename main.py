0597e71aad7f3b63094076e367daf8ebd2501ce3
cadencranford12133 <caden.cranford@ptaaschool.net>
2022-04-11 08:40:43 -0500

    dy,

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from base64 import encode
+from email import message
 import tkinter as tk
 
 root = tk.Tk()
 root.title('Caesar Cipher')
 root.geometry('600x600')
 
-
-
+# 1st row of information
+message_label = tk.label(
+    root,
+    text= 'Enter an message to encode:',
+    font=('arial 16 bold'),
+    bg='red',
+    fg='#FF0'
+)
+message_label.grid(row=0,colum=zero)
 root.mainloop()
