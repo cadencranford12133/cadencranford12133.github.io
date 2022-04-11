cbd16131c2f61ee59ed9f86d8ba07b80bc39f8f7
cadencranford12133 <caden.cranford@ptaaschool.net>
2022-04-11 08:57:13 -0500

    vghcjhjhhj

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title('Caesar Cipher')
 root.geometry('600x600')
+root.columnconfigure(1,weight=1)
 
 # 1st row of information
 message_Label = tk.Label(
@@ -20,6 +21,6 @@ message_input = tk.Entry(
     root,
     textvariable=message_var
 )
-message_input.grid(row=0,column=0)
+message_input.grid(row=0,column=1,sticky=(tk.N,tk.E,tk.W,tk.S),padx=)
 
 root.mainloop()
